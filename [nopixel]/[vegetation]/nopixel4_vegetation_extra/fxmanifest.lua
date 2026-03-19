cc4fd7a22d6e2da4af063d95577018ebcbffa486
Brandon <lockedaxev3@outlook.com>
2026-03-19 13:47:49 -0500

    Add more plants

---
+++ [nopixel]/[vegetation]/nopixel4_vegetation_extra/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Razed'
+description 'nopixel 4.0 - Vegetation Extra'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'nve_vegetation_extra_cache_y.dat'
+}
+
+escrow_ignore {
+    'stream/**/*.ytd'
+  }
+  
+lua54 'yes'
+dependency '/assetpacks'
